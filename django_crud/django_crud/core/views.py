d4bc487c8ea5131f0784393927843638e571fcb7
José Anderson <jose.andersonjr12@gmail.com>
2022-08-16 11:46:36 -0300

    Configurada primeiras url e o objeto produto com seus atributos

--- django_crud/django_crud/core/views.py
+++ django_crud/django_crud/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
 # Create your views here.
+def list_products(request):
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request,'products.html', context=context)
+
+def create_products(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_products')
+    
+    return render(request, 'products')
