0846154cb183bd85a45bf9054bd556cbb10979dc
José Anderson <jose.andersonjr12@gmail.com>
2022-08-17 14:17:27 -0300

    Finalização do projeto e estudo

--- django_crud/django_crud/core/urls.py
+++ django_crud/django_crud/core/urls.py
@@ -1,11 +1,13 @@
 
 from unicodedata import name
 from django.urls import re_path, path
-from .views import list_products
+from .views import list_products, create_products, update_products, delete_products
 
 
 urlpatterns = [
-    path('', list_products, name='list_projects'),
-    path('new_product', create_products, name='create_products')
+    path('', list_products, name='list_products'),
+    path('new_product', create_products, name='create_products'),
+    path('update/<int:id>/', update_products, name='update_products'),
+    path('delete/<int:id>/', delete_products, name='delete_products')
 
 ]
--- django_crud/django_crud/core/views.py
+++ django_crud/django_crud/core/views.py
@@ -1,5 +1,7 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def list_products(request):
@@ -16,4 +18,32 @@ def create_products(request):
         form.save()
         return redirect('list_products')
     
-    return render(request, 'products')
+    context = {
+        'form':form
+    }
+    return render(request, 'products_form.html', context=context)
+
+def update_products(request, id):
+    product = Product.objects.get(id=id)
+    form = ProductForm(request.POST or None, instance=product) #instance product serve para que o form já venha como instância de produto e preenchido
+    if form.is_valid():
+        form.save()
+        return redirect('list_products')
+
+    context = {
+        'form':form,
+        'product': product
+    }
+    return render(request, 'products_form.html', context=context)
+
+def delete_products(request, id):
+    product = Product.objects.get(id=id)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('list_products')
+
+    context = {
+        'product': product
+    }
+    return render(request, 'prod_delete_confirm.html', context=context)
